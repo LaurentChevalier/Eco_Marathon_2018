81036f10d03c058cec3b0177fa699f9144a59ae6
Laurent Chevalier <33724778+LaurentChevalier@users.noreply.github.com>
2018-04-04 19:24:44 +0200

    Visual Basic code to display and save motor test bench measurements

---
+++ Motor_Test_Bench_VB/Motor_Test_Bench/Main.vb
@@ -0,0 +1,115 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class Main
+    Dim Tension As Decimal
+    Dim Courant As Decimal
+    Dim Puissance As Decimal
+    Dim Couple As Decimal
+    Dim Vitesse As Decimal
+    Shared _serialPort As SerialPort
+    Private Shared WithEvents myTimer As New System.Windows.Forms.Timer()
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.Text = "EM Motor Test Bench"
+        Me.BackColor = Color.Goldenrod
+        Timer1.Enabled = True
+        Label1.Text = "Battery Voltage"
+        Label2.Text = "Motor Current"
+        Label3.Text = "Speed"
+        Label4.Text = "Torque"
+        Label5.Text = "Power"
+        Button1.Text = "Save to CSV"
+        Button2.Text = "Quit"
+
+        SerialPort1.DataBits = 8
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Handshake = Handshake.None
+        SerialPort1.Encoding = System.Text.Encoding.Default
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        MsgBox("Thanks to use this application", 64, "BYE")
+        SerialPort1.Close()
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        'CreateCSVfile(Application.StartupPath & "\" & "CustomerRecords.csv", txtCustomerId.Text.ToString(), txtFirstName.Text.ToString(), txtLastName.Text.ToString(), txtPhoneNo.Text.ToString(), txtEmail.Text.ToString(), txtMessage.Text.ToString())
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        SerialPort1.Open()
+
+        'edt_vitesse.Text = SerialPort1.ReadLine
+        Dim testString1 As String = SerialPort1.ReadLine
+        Dim CourantVal As String = String.Empty
+        Dim TensionVal As String = String.Empty
+        Dim VitesseVal As String = String.Empty
+        Dim CoupleVal As String = String.Empty
+        Dim liste() As Char = testString1.ToCharArray
+        Dim CompteurChar As Integer = 0
+
+        For Each ValeurChar In liste
+            If (CompteurChar > 0 And CompteurChar <= 4) Then
+                TensionVal += ValeurChar
+
+            End If
+            If (CompteurChar > 7 And CompteurChar <= 11) Then
+                CourantVal += ValeurChar
+
+            End If
+            If (CompteurChar > 15 And CompteurChar <= 19) Then
+                VitesseVal += ValeurChar
+
+            End If
+            If (CompteurChar > 22 And CompteurChar <= 26) Then
+                CoupleVal += ValeurChar
+
+            End If
+            CompteurChar += 1
+        Next
+
+        Tension = Convert.ToDecimal(TensionVal)
+        Courant = Convert.ToDecimal(CourantVal)
+        Vitesse = Convert.ToDecimal(VitesseVal)
+        Couple = Convert.ToDecimal(CoupleVal)
+        Tension = ((55000 * (Tension)) / 1023) / 1000
+        Tension = Decimal.Round(Tension, 3)
+        Courant = ((((5000 * (Courant)) / 1023) - 2500) * (1000 / 66)) / 1000
+        Courant = Decimal.Round(Courant, 3)
+        Puissance = Tension * Courant
+        TextBox1.Text = Tension
+        TextBox2.Text = Courant
+        TextBox3.Text = VitesseVal
+        TextBox4.Text = CoupleVal
+        TextBox5.Text = Puissance
+
+        SerialPort1.Close()
+    End Sub
+
+    'Private Sub CreateCSVfile(ByVal _strCustomerCSVPath As String, ByVal _CustomerID As String, ByVal _FirstName As String, ByVal _LastName As String, ByVal _Phone As String, ByVal _Email As String, ByVal _Msg As String)
+    '    Try
+    '        Dim stLine As String = ""
+    '        Dim objWriter As IO.StreamWriter = IO.File.AppendText(_strCustomerCSVPath)
+    '        If IO.File.Exists(_strCustomerCSVPath) Then
+    '            objWriter.Write(_CustomerID & ",")
+    '            objWriter.Write(_FirstName & ",")
+    '            objWriter.Write(_LastName & ",")
+    '            objWriter.Write(_Email & ",")
+
+    '            'If value contains comma in the value then you have to perform this opertions
+    '            Dim append = If(_Msg.Contains(","), String.Format("""{0}""", _Msg), _Msg)
+    '            stLine = String.Format("{0}{1},", stLine, append)
+    '            objWriter.Write(stLine)
+
+    '            objWriter.Write(_Phone)
+    '            objWriter.Write(Environment.NewLine)
+    '        End If
+    '        objWriter.Close()
+    '    Catch ex As Exception
+    '    End Try
+    'End Sub
+End Class
---
+++ Motor_Test_Bench_VB/Motor_Test_Bench/Settings.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Settings
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(114, 67)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(114, 128)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 131)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Button1
+        '
+        Me.Button1.AutoSize = True
+        Me.Button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button1.Location = New System.Drawing.Point(100, 194)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.AutoSize = True
+        Me.Button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button2.Location = New System.Drawing.Point(218, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(54, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Settings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Settings"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
